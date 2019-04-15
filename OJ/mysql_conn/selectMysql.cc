eabb85e35641c363a8aec279e080a3a2672cb8f3
Gzmy <1370711580@qq.com>
2019-04-15 21:01:38 +0800

    delete others

--- OJ/mysql_conn/selectMysql.cc
+++
@@ -1,9 +0,0 @@
-#include "comm.h"
-
-int main()
-{
-	MYSQL *my_fd = connectMysql();
-	selectMysql(my_fd);
-	closeMysql(my_fd);
-	return 0;
-}
--- OJ/mysql_conn/test.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "mysql.h"
-
-int main()
-{
-	std::cout << "mysql version is" << mysql_get_client_info() << std::endl;
-	return 0;
-}
